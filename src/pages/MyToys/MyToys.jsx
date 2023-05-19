fa38a035127e598b7125dd19e7ceec7aa3f3861e
Syed-Anwarul-Haque-Piash <syed88783@gmail.com>
2023-05-19 19:24:54 +0600

    my toy

--- src/pages/MyToys/MyToys.jsx
+++ src/pages/MyToys/MyToys.jsx
@@ -1,52 +1,53 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../../providers/AuthProvider';
+import { key } from 'localforage';
 
 const MyToys = () => {
-    const {user}=useContext(AuthContext);
-    const [toys,setToys]=useState([])
-    useEffect(()=>{
+    const { user } = useContext(AuthContext);
+    const [toys, setToys] = useState([])
+    useEffect(() => {
         fetch(`http://localhost:5000/mytoys/${user?.email}`)
-        .then(res=>res.json())
-        .then(data=>setToys(data))
-    },[user])
+            .then(res => res.json())
+            .then(data => setToys(data))
+    }, [user])
+    const handleDelete=(id)=>{
+        console.log("deleted")
+    }
     return (
         <div className="overflow-x-auto">
-  <table className="table table-compact w-full">
-    <thead>
-      <tr>
-        <th></th> 
-        <th>Name</th> 
-        <th>Job</th> 
-        <th>company</th> 
-        <th>location</th> 
-        <th>Last Login</th> 
-        <th>Favorite Color</th>
-      </tr>
-    </thead> 
-    <tbody>
-      <tr>
-        <th>1</th> 
-        <td>Cy Ganderton</td> 
-        <td>Quality Control Specialist</td> 
-        <td>Littel, Schaden and Vandervort</td> 
-        <td>Canada</td> 
-        <td>12/16/2020</td> 
-        <td>Blue</td>
-      </tr>
-      
-      
-      
-      
-      
-      
-     
-    
-     
-      
-    </tbody> 
-   
-  </table>
-</div>
+            <table className="table table-compact w-full">
+                <thead>
+                    <tr>
+                    <th>Seller</th>
+                        <th>Toy Name</th>
+                        <th>Category</th>
+                        <th>Price</th>
+                        <th>Quantity</th>
+                        <th>Action</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {toys.map(toy=>
+                        
+                        <tr>
+                        
+                        <th>{toy?.postedBy}</th>
+                        <td>{toy?.toyname}</td>
+                        <td>{toy?.category}</td>
+                        <td>{toy?.price}</td>
+                        <td>{toy?.quantity}</td>
+                        <td>
+                        <button className="btn btn-info">Update</button>
+                        </td>
+                        <td>
+                        <button onClick={()=>handleDelete(toy?._id)} className="btn btn-error">Delete</button>
+                        </td>
+                    </tr>)}
+                </tbody>
+
+            </table>
+        </div>
     );
 };
 
