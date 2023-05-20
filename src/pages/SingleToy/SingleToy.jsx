ca923a25696082fe520edf0bb5701ef6012c6051
Syed-Anwarul-Haque-Piash <syed88783@gmail.com>
2023-05-20 21:53:35 +0600

    react rating

--- src/pages/SingleToy/SingleToy.jsx
+++ src/pages/SingleToy/SingleToy.jsx
@@ -1,28 +1,39 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import Rating from "react-rating";
+import { FaRegStar, FaStar } from 'react-icons/fa';
 
 const SingleToy = () => {
-    const toy = useLoaderData();
-    console.log(toy)
-    const {image,toyname,sellername,postedBy,category,price,rating,quantity,description}=toy;
-    return (
-        <div className="card  glass">
-        <figure><img style={{width:"700px",height:"500px"}} src={image} alt="car!"/></figure>
-        <div className="card-body ms-10">
-          <h2 className="card-title">Name: {toyname}</h2>
-          <p>Description: {description}</p>
-          <p>Seller Name:{sellername}</p>  
-          <p>Price: ${price}</p>
-          <p>Category: {category}</p>
-          <p>Rating: {rating}</p>
-          <p>Posted By : {postedBy}</p>
-          <p>Quantity: {quantity}</p>
-          {/* <div className="card-actions justify-end">
+  const toy = useLoaderData();
+  console.log(toy)
+  const { image, toyname, sellername, postedBy, category, price, rating, quantity, description } = toy;
+  return (
+    <div className="card  glass">
+      <figure><img style={{ width: "700px", height: "500px" }} src={image} alt="car!" /></figure>
+      <div className="card-body ms-10">
+        <h2 className="card-title">Name: {toyname}</h2>
+        <p>Description: {description}</p>
+        <p>Seller Name:{sellername}</p>
+        <p>Price: ${price}</p>
+        <p>Category: {category}</p>
+        {/* <p>Rating: {rating}</p> */}
+        <Rating
+          placeholderRating={rating}
+          readonly
+          emptySymbol={<FaRegStar></FaRegStar>}
+          placeholderSymbol={
+            <FaStar className="text-warning"></FaStar>
+          }
+          fullSymbol={<FaStar></FaStar>}
+        ></Rating>
+        <p>Posted By : {postedBy}</p>
+        <p>Quantity: {quantity}</p>
+        {/* <div className="card-actions justify-end">
             <button className="btn btn-primary">Learn now!</button>
           </div> */}
-        </div>
       </div>
-    );
+    </div>
+  );
 };
 
 export default SingleToy;
--- src/pages/Toy/Toy.jsx
+++ src/pages/Toy/Toy.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Rating from "react-rating";
+import { FaRegStar, FaStar } from 'react-icons/fa';
 
 const Toy = ({ toy }) => {
     const { _id,image, price, rating, toyname } = toy || {};
@@ -9,7 +11,16 @@ const Toy = ({ toy }) => {
             <div className="card-body">
                 <h2 className="card-title">{toyname}</h2>
                 <p>${price}</p>
-                <p>{rating}</p>
+                {/* <p>{rating}</p> */}
+                <Rating
+                      placeholderRating={rating}
+                      readonly
+                      emptySymbol={<FaRegStar></FaRegStar>}
+                      placeholderSymbol={
+                        <FaStar className="text-warning"></FaStar>
+                      }
+                      fullSymbol={<FaStar></FaStar>}
+                    ></Rating>
                 <div className="card-actions justify-end">
                     <Link to={`singleToy/${_id}`}>
                         <button className="button">View Details</button>
